ea4e1ecd57a827ca470d215a7ff82d1d674b270d
Teelek0301 <tanteelek123456789@gmail.com>
2023-12-07 00:52:56 +0800

    finised nav bar and added footer component

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,16 @@
 import HamburgerMenu from "./components/hambuger_menu";
+import Footer from "./components/footer";
 
 function App() {
   return (
     <div>
         <HamburgerMenu />
+        <div>
+          <h1>
+            Hello world!
+          </h1>
+        </div>
+        <Footer/>
     </div>
   );
 }
--- client/src/components/hambuger_menu.js
+++ client/src/components/hambuger_menu.js
@@ -1,42 +1,49 @@
 import React, { useState } from "react";
-import { Navbar, Nav, NavDropdown, NavbarBrand } from "react-bootstrap";
+import { Navbar, Nav, NavbarBrand } from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.css";
 import { NavLink } from "react-router-dom";
+import { FaUser } from "react-icons/fa";
+import "./hamburger_menu.css";
 
-export default function HamburgerMenu() {
+function HamburgerMenu() {
     const [expanded, setExpanded] = useState(false);
 
     return (
         <div>
-            
-            <Navbar bg="light" expand="lg">
-                
+
+            <Navbar bg="light" expand="" className="yellow-border-navbar">
+
                 <Navbar.Toggle aria-controls="basic-navbar-nav" onClick={() => setExpanded(!expanded)} />
-                <NavbarBrand className="mx-auto" href="#" >
-                <span class="navbar-brand mb-0 h1">TaeKwonConnect</span>
+
+                <NavbarBrand className="mx-5" href="#" >
+                    <span class="navbar-brand mb-0 h1">TaeKwonConnect</span>
                 </NavbarBrand>
+                <FaUser size={20} onClick={() => { console.log("user clicked"); }}></FaUser>
                 <Navbar.Collapse id="basic-navbar-nav">
-                    <Nav className="ml-auto">
-                        <Nav.Link href="#home" onClick={() => setExpanded(false)}>
+                    <Nav>
+                        <Nav.Link href="#" onClick={() => { setExpanded(false); console.log("home clicked"); }}>
                             Home
                         </Nav.Link>
-                        <Nav.Link href="#" onClick={() => setExpanded(false)}>
+                        <Nav.Link href="#" onClick={() => { setExpanded(false); console.log("members clicked"); }}>
                             Members
                         </Nav.Link>
-                        <Nav.Link href="#" onClick={() => setExpanded(false)}>
+                        <Nav.Link href="#" onClick={() => { setExpanded(false); console.log("achievement clicked"); }}>
                             Achievements
                         </Nav.Link>
-                        <Nav.Link href="#" onClick={() => setExpanded(false)}>
+                        <Nav.Link href="#" onClick={() => { setExpanded(false); console.log("news clicked"); }}>
                             News
                         </Nav.Link>
                         <Nav.Link >
-                            
+
                         </Nav.Link>
                     </Nav>
                 </Navbar.Collapse>
-                
+
+
             </Navbar>
         </div>
 
     );
 }
+
+export default HamburgerMenu;
