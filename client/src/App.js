722de3ced8af3d0de0f60a59382d59e0b1847fd7
Teelek0301 <tanteelek123456789@gmail.com>
2024-01-11 16:46:36 +0800

    latest V

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,17 @@
-import HamburgerMenu from "./components/hambuger_menu";
-import Footer from "./components/footer";
-import Home from "./components/home_page";
+import HamburgerMenu from "./components/hamburgerMenu/hambuger_menu";
+import Footer from "./components/footer/footer";
+import Home from "./components/home/home_page";
 import React from "react";
-import Achievement from "./components/achievement_page";
-import Members from "./components/members_page";
-import News from "./components/news_page";
-import SignUp from "./components/signup_page";
-import SelectedMember from "./components/selectedMember_page";
-import SelectedCoach from "./components/selectedCoach_page";
-import SelectedExco from "./components/selectedExco_page";
-import EditCoach from "./components/editCoach_page";
-import EditExco from "./components/editExco_page";
-import EditMember from "./components/editMember_Page";
+import Achievement from "./components/achievement/achievement_page";
+import Members from "./components/member/members_page";
+import News from "./components/news/news_page";
+import SignUp from "./components/signup/signup_page";
+import SelectedMember from "./components/selectedMember/selectedMember_page";
+import SelectedCoach from "./components/selectedCoach/selectedCoach_page";
+import SelectedExco from "./components/selectedExco/selectedExco_page";
+import EditCoach from "./components/editCoach/editCoach_page";
+import EditExco from "./components/editExco/editExco_page";
+import EditMember from "./components/editMember/editMember_Page";
 import { Route, Routes } from "react-router-dom";
 
 
